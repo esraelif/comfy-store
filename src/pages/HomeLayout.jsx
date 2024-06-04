d2102bd727d3c8e5e18f0395b9e693df34f6094e
esraelif <elifesratunca@gmail.com>
2024-06-04 17:29:40 +0200

    "Updated About, Error, HomeLayout, Landing, and Login pages with new content and layout changes."

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const HomeLayout = () => {
     return (
-        <div>
+        <>
+            <nav>
+                <span className='text-4xl text-primary'>Comfy</span>
+            </nav>
+            <Outlet />
 
-        </div>
+        </>
     );
 }
 
