dde55ae5d8a6b37bbd04f38e3bfbd8f6ca309750
esraelif <elifesratunca@gmail.com>
2024-06-06 17:15:32 +0200

    "Added Header and Navbar exports to components/index.js and imported/used them in HomeLayout.jsx, replacing existing nav element."

---
+++ src/components/NavLinks.jsx
@@ -0,0 +1,30 @@
+import { NavLink } from "react-router-dom";
+
+const links = [
+    { id: 1, text: "Home", url: "/" },
+    { id: 2, text: "About", url: "about" },
+    { id: 3, text: "Products", url: "products" },
+    { id: 4, text: "Cart", url: "cart" },
+    { id: 5, text: "Checkout", url: "checkout" },
+    { id: 6, text: "Orders", url: "orders" },
+]
+
+const NavLinks = () => {
+    return (
+        <>
+            {links.map((link) => {
+                const { id, url, text } = link
+                return (
+                    <li key={id}>
+                        <NavLink className='capitalize' to={url}>
+                            {text}
+                        </NavLink>
+                    </li>
+                )
+            })}
+
+        </>
+    );
+}
+
+export default NavLinks;
