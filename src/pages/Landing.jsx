d41b3cc7b5ac4dd9748adf718c78df8a5d306122
esraelif <elifesratunca@gmail.com>
2024-06-07 14:25:33 +0200

    "Added Hero component export and import, updated Landing page to use Hero component"

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import { Hero } from '../components';
 
 const Landing = () => {
     return (
-        <div>
-            Landing
-        </div>
+        <>
+            <Hero />
+        </>
     );
 }
 
