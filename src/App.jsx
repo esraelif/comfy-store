b4f60520e678200a5e7680c1e2589144b102f258
esraelif <elifesratunca@gmail.com>
2024-06-07 16:10:26 +0200

    "Updated App.jsx to integrate React Query and Redux, modified Navbar.jsx to use Redux for theme toggling, refactored ProductsGrid.jsx to display product information, and updated various other components and utilities."

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-import { About, Cart, Checkout, Error, HomeLayout, Landing, Login, Orders, Products, Register, SingleProduct } from './pages';
-
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from './pages';
+
 import { ErrorElement } from './components';
-//*loaders
+
+// loaders
 import { loader as landingLoader } from './pages/Landing';
-//*actions
+import { loader as singleProductLoader } from './pages/SingleProduct';
+import { loader as productsLoader } from './pages/Products';
+import { loader as checkoutLoader } from './pages/Checkout';
+import { loader as ordersLoader } from './pages/Orders';
+// actions
+import { action as registerAction } from './pages/Register';
+import { action as loginAction } from './pages/Login';
+import { action as checkoutAction } from './components/CheckoutForm';
+import { store } from './store';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -16,56 +47,61 @@ const router = createBrowserRouter([
         index: true,
         element: <Landing />,
         errorElement: <ErrorElement />,
-        loader: landingLoader
+        loader: landingLoader(queryClient),
       },
       {
         path: 'products',
-        element: <Products />
+        element: <Products />,
+        errorElement: <ErrorElement />,
+        loader: productsLoader(queryClient),
       },
       {
         path: 'products/:id',
-        element: <SingleProduct />
+        element: <SingleProduct />,
+        errorElement: <ErrorElement />,
+        loader: singleProductLoader(queryClient),
       },
       {
         path: 'cart',
-        element: <Cart />
+        element: <Cart />,
       },
       {
         path: 'about',
-        element: <About />
+        element: <About />,
       },
       {
         path: 'checkout',
-        element: <Checkout />
+        element: <Checkout />,
+        loader: checkoutLoader(store),
+        action: checkoutAction(store, queryClient),
       },
       {
         path: 'orders',
-        element: <Orders />
-      }
-    ]
-
+        element: <Orders />,
+        loader: ordersLoader(store, queryClient),
+      },
+    ],
   },
   {
     path: '/login',
     element: <Login />,
-    errorElement: <Error />
-
+    errorElement: <Error />,
+    action: loginAction(store),
   },
   {
     path: '/register',
     element: <Register />,
-    errorElement: <Error />
-
-  }
-
-
-])
+    errorElement: <Error />,
+    action: registerAction,
+  },
+]);
 
 const App = () => {
   return (
-    <RouterProvider router={router} />
-
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
-}
-
+};
 export default App;
